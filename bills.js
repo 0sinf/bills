a53bc445d02980a9ad76869716425ea266d82d7b
정세영 <wjdtp93@gmail.com>
2022-06-06 11:57:16 +0900

    style: Fix function location

--- bills.js
+++ bills.js
@@ -1,36 +1,5 @@
-"use strict";
-
 const fs = require("fs");
 const WAGE = 10000;
-
-function checkTimeValue(start, end, hour, min) {
-  if (start !== Number(hour)) {
-    console.log(`${start} doesn't not equal ${hour} in HOUR.`);
-  }
-
-  if (end === 0 && min) {
-    console.log(`Minute isn't zero.`);
-  }
-
-  if (end !== 0 && end !== Number(min)) {
-    console.log(`${end} doesn't not equal ${min} in HOUR.`);
-  }
-}
-
-function getHourAndMin(start, end) {
-  const [startHour, startMin] = start.split(":").map(Number);
-  const [endHour, endMin] = end.split(":").map(Number);
-
-  let h = endHour - startHour;
-  let m = endMin - startMin;
-
-  if (endMin - startMin < 0) {
-    m = 60 + m;
-    h = h - 1;
-  }
-
-  return [h, m];
-}
 
 function main() {
   const bills = fs.readFileSync("./bills.txt", { encoding: "utf-8" });
@@ -46,7 +15,7 @@ function main() {
         .replace("분", "")
         .split(" ");
 
-      const [h, m] = getHourAndMin(start, end);
+      const [h, m] = getHourAndMinByTime(start, end);
 
       checkTimeValue(h, m, hour, min);
 
@@ -60,6 +29,10 @@ function main() {
   totalH += Math.floor(totalM / 60);
   totalM = totalM % 60;
 
+  getResult(totalH, totalM);
+}
+
+function getResult(totalH, totalM) {
   console.log(`Total time: ${totalH}시간 ${totalM}분`);
 
   console.log(
@@ -68,6 +41,47 @@ function main() {
       Math.floor((totalM / 60) * WAGE)
     ).toLocaleString("ko-kr")}원`
   );
+}
+
+function getHourAndMinByTime(start, end) {
+  const [startHour, startMin] = start.split(":").map(Number);
+  const [endHour, endMin] = end.split(":").map(Number);
+
+  let h = endHour - startHour;
+  let m = endMin - startMin;
+
+  if (endMin - startMin < 0) {
+    m = 60 + m;
+    h = h - 1;
+  }
+
+  return [h, m];
+}
+
+function checkTimeValue(h, m, hour, min) {
+  if (isNotEqualHour(h, hour)) {
+    console.log(`${h} doesn't not equal ${hour} in HOUR.`);
+  }
+
+  if (endIsZeroButMinIsNotZero(m, min)) {
+    console.log(`Minute isn't zero.`);
+  }
+
+  if (isNotEqualMin(m, min)) {
+    console.log(`${m} doesn't not equal ${min} in HOUR.`);
+  }
+}
+
+function isNotEqualHour(h, hour) {
+  return h !== Number(hour);
+}
+
+function endIsZeroButMinIsNotZero(m, min) {
+  return m === 0 && min;
+}
+
+function isNotEqualMin(m, min) {
+  return m !== 0 && m !== Number(min);
 }
 
 main();
