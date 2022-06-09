e23a75b369995f4da5a28ae8d8646681e49d0b14
정세영 <wjdtp93@gmail.com>
2022-06-09 21:25:19 +0900

    refactor: Fix element

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function display() {
+function displayInit() {
   const container = document.getElementById("container");
+  container.innerHTML = "";
 
   const h = document.createElement("h1");
   h.classList.add("title");
   h.innerText = "근무 시간 입력하기";
 
-  const article = document.createElement("article");
-  article.classList.add("content");
-  article.id = "content";
+  const content = document.createElement("article");
+  content.classList.add("content");
+  content.id = "content";
 
   const textarea = document.createElement("textarea");
   textarea.classList.add("textarea");
@@ -16,23 +17,14 @@ function display() {
 
   const button = document.createElement("button");
   button.classList.add("button");
-  button.onclick = () => calculate();
+  button.onclick = () => displayResult(textarea.value);
   button.innerText = "총 근무 시간 확인";
 
-  article.append(textarea, button);
-  container.append(h, article);
+  content.append(textarea, button);
+  container.append(h, content);
 }
 
-function createButton() {
-  const button = document.createElement("button");
-  button.classList.add("button");
-  button.onclick = () => display();
-  button.innerText = "다시 입력하기";
-
-  return button;
-}
-
-function getTable(trs) {
+function getTable(tbody) {
   const tb = document.createElement("div");
   tb.classList.add("table");
 
@@ -40,18 +32,16 @@ function getTable(trs) {
   th.classList.add("table__head");
 
   const headItems = ["날짜", "요일", "출근 시간", "퇴근 시간", "시간"];
-  const tds = headItems.map((item) => {
+  headItems.forEach((item) => {
     const td = document.createElement("div");
     td.classList.add("table__items");
     td.innerText = item;
-    return td;
+    th.append(td);
   });
 
-  th.append(tds);
-
-  tb.append(th, trs);
+  tb.append(th, tbody);
 
   return tb;
 }
 
-display();
+displayInit();
