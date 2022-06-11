bd4d475f19fbe1b33eb312d164322c0d5a08d803
정세영 <wjdtp93@gmail.com>
2022-06-11 09:35:57 +0900

    feat: Set createElem

--- index.js
+++ index.js
@@ -1,52 +1,10 @@
-function displayInit() {
-  const container = document.getElementById("container");
-  container.innerHTML = "";
-
-  const h = document.createElement("h1");
-  h.classList.add("title");
-  h.innerText = "근무 시간 입력하기";
-
-  const content = document.createElement("article");
-  content.classList.add("content");
-  content.id = "content";
-
-  const textarea = document.createElement("textarea");
-  textarea.classList.add("textarea");
-  textarea.id = "times";
-  textarea.placeholder = "예시) 6/7 화 11:00 18:00";
-
-  const button = document.createElement("button");
-  button.classList.add("button");
-  button.onclick = () => displayResult(textarea.value);
-  button.innerText = "총 근무 시간 확인";
-
-  content.append(textarea, button);
-  container.append(h, content);
-}
-
-function getTable(tbody) {
-  const tb = document.createElement("div");
-  tb.classList.add("table");
-
-  const th = document.createElement("div");
-  th.classList.add("table__head");
-
-  const headItems = ["날짜", "요일", "출근 시간", "퇴근 시간", "시간"];
-  headItems.forEach((item) => {
-    const td = document.createElement("div");
-    td.classList.add("table__items");
-    td.innerText = item;
-    th.append(td);
-  });
-
-  tb.append(th, tbody);
-
-  return tb;
-}
-
 function createElem(type, className, option) {
   const elem = document.createElement(type);
   elem.classList.add(className);
+
+  if (!option) {
+    return elem;
+  }
 
   Object.entries(option).forEach(([key, value]) => {
     elem[key] = value;
@@ -55,38 +13,69 @@ function createElem(type, className, option) {
   return elem;
 }
 
+function displayInit() {
+  const container = document.getElementById("container");
+  container.innerHTML = "";
+
+  const h = createElem("h1", "title", { innerText: "근무 시간 입력하기" });
+
+  const content = createElem("article", "content");
+
+  const textarea = createElem("textarea", "textarea", {
+    placeholder: "예시) 6/7 화 11:00 18:00",
+  });
+
+  const button = createElem("button", "button", {
+    onclick: () => displayResult(textarea.value),
+    innerText: "총 근무 시간 확인",
+  });
+
+  content.append(textarea, button);
+  container.append(h, content);
+}
+
+function getTable(tbody) {
+  const tb = createElem("div", "table");
+
+  const th = createElem("div", "table__head");
+
+  const headItems = ["날짜", "요일", "출근 시간", "퇴근 시간", "시간"];
+  headItems.forEach((item) => {
+    const td = createElem("div", "table__items", { innerText: item });
+    th.append(td);
+  });
+
+  tb.append(th, tbody);
+
+  return tb;
+}
+
 function displayResult(times) {
   const container = document.getElementById("container");
   container.innerHTML = "";
 
   const [totalH, totalM, tbody, day] = calculate(times);
 
-  const h = document.createElement("h1");
-  h.classList.add("title");
-  h.innerText = "입력 결과 출력";
+  const h = createElem("h1", "title", { innerText: "입력 결과 출력" });
 
-  const content = document.createElement("div");
-  content.classList.add("content");
+  const content = createElem("div", "content");
   content.append(getTable(tbody));
 
   const result = getResultMessage(totalH, totalM, day);
 
-  const button = document.createElement("button");
-  button.classList.add("button");
-  button.onclick = () => displayInit();
-  button.innerText = "다시 입력하기";
+  const button = createElem("button", "button", {
+    onclick: () => displayInit(),
+    innerText: "다시 입력하기",
+  });
 
   container.append(h, content, result, button);
 }
 
 function getTableRow(items) {
-  const tr = document.createElement("div");
-  tr.classList.add("table__row");
+  const tr = createElem("div", "table__row");
 
   items.forEach((item) => {
-    const td = document.createElement("div");
-    td.classList.add("table__items");
-    td.innerText = item;
+    const td = createElem("div", "table__items", { innerText: item });
     tr.append(td);
   });
 
@@ -94,9 +83,9 @@ function getTableRow(items) {
 }
 
 function getResultMessage(totalH, totalM, day) {
-  const result = document.createElement("div");
-  result.classList.add("result");
-  result.innerText = `Total time: 총 ${day}일 간 ${totalH}시간 ${totalM}분`;
+  const result = createElem("div", "result", {
+    innerText: `Total time: 총 ${day}일 간 ${totalH}시간 ${totalM}분`,
+  });
   return result;
 }
 
