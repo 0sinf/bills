28b3b09c216bce2d7b02b1fa09538a956f43a716
정세영 <wjdtp93@gmail.com>
2022-07-09 13:07:52 +0900

    chore: Add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,10 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  entry: path.resolve(__dirname, "src", "index.ts"),
   module: {
     rules: [
+      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
       { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
     ],
   },
@@ -14,4 +16,11 @@ module.exports = {
       template: path.resolve(__dirname, "src", "index.html"),
     }),
   ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
