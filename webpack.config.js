cd1b60704885da14923b249d6e42e0189c7185f4
정세영 <wjdtp93@gmail.com>
2022-07-09 12:59:14 +0900

    chore: Add CSS loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
-const HTMLWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  module: {
+    rules: [
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+    ],
+  },
   plugins: [
+    new MiniCssExtractPlugin({ filename: "index.css" }),
     new HTMLWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
     }),
