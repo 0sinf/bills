a7d41b745d45e44478768f54618de1cef3a3088b
정세영 <wjdtp93@gmail.com>
2022-07-16 10:00:20 +0900

    chore: Add table

---
+++ src/components/Table.ts
@@ -0,0 +1,7 @@
+import createElement from "../utils/create-elem";
+
+export default function Table() {
+  const table = createElement("table", {}, null);
+
+  return table;
+}
--- src/pages/Result.ts
+++ src/pages/Result.ts
@@ -2,15 +2,15 @@ import createElement from "../utils/create-elem";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import { go } from "../router/index";
+import Article from "../components/Article";
+import Table from "../components/Table";
 
 export default function Result() {
   const container = createElement("div", { className: ["container"] }, [
     Header("입력 결과 출력"),
+    Article([Table()]),
+    Button({ onclick: go("/"), value: "다시 입력하기" }),
   ]);
-
-  const button = Button({ onclick: go("/"), value: "다시 입력하기" });
-
-  container.append(button);
 
   return container;
 }
