38a844bf16f56af261d129be7f4e6269d08fe942
정세영 <wjdtp93@gmail.com>
2022-07-16 10:07:57 +0900

    feat: Add Table head

--- src/components/Table.ts
+++ src/components/Table.ts
@@ -1,7 +1,10 @@
 import createElement from "../utils/create-elem";
+import TableHead from "./TableHead";
 
 export default function Table() {
-  const table = createElement("table", {}, null);
+  const table = createElement("div", { className: ["table"] }, [
+    TableHead(["날짜", "요일", "출근 시간", "퇴근 시간", "시간"]),
+  ]);
 
   return table;
 }
