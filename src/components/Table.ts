59604cecbee3a0148a24b61e78ddc409ca8aedd4
정세영 <wjdtp93@gmail.com>
2022-07-16 10:18:03 +0900

    chore: Add to do

--- src/components/Table.ts
+++ src/components/Table.ts
@@ -1,9 +1,15 @@
 import createElement from "../utils/create-elem";
 import TableHead from "./TableHead";
+import TableRow from "./TableRow";
 
 export default function Table() {
+  // FIXME: textarea로 입력된 데이터 받아오기
+  const records = [];
+
   const table = createElement("div", { className: ["table"] }, [
     TableHead(["날짜", "요일", "출근 시간", "퇴근 시간", "시간"]),
+    // TODO: Add body with Row
+    ...records.map((record) => TableRow(record)),
   ]);
 
   return table;
