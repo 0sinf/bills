64f8e2f845b0b5f04fc40031cf013b9935358397
정세영 <wjdtp93@gmail.com>
2022-07-16 09:44:45 +0900

    feat: Add button

--- src/pages/Result.ts
+++ src/pages/Result.ts
@@ -1,10 +1,16 @@
 import createElement from "../utils/create-elem";
 import Header from "../components/Header";
+import Button from "../components/Button";
+import { go } from "../router/index";
 
 export default function Result() {
   const container = createElement("div", { className: ["container"] }, [
     Header("입력 결과 출력"),
   ]);
+
+  const button = Button({ onclick: go("/"), value: "다시 입력하기" });
+
+  container.append(button);
 
   return container;
 }
