d0eba1473768f814a3c02f0b68877b72f0e484e9
정세영 <wjdtp93@gmail.com>
2022-07-16 10:20:12 +0900

    chore: Add mock

--- src/components/Table.ts
+++ src/components/Table.ts
@@ -4,7 +4,10 @@ import TableRow from "./TableRow";
 
 export default function Table() {
   // FIXME: textarea로 입력된 데이터 받아오기
-  const records = [];
+  const records = [
+    ["d", "d", "d", "d", "d"],
+    ["d", "d", "d", "d", "d"],
+  ];
 
   const table = createElement("div", { className: ["table"] }, [
     TableHead(["날짜", "요일", "출근 시간", "퇴근 시간", "시간"]),
