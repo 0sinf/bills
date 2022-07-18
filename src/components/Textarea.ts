f2d1f314039ad5d8d03f0147365fe9e9653238fb
정세영 <wjdtp93@gmail.com>
2022-07-18 13:25:23 +0900

    feat: Add handle input

--- src/components/Textarea.ts
+++ src/components/Textarea.ts
@@ -1,9 +1,22 @@
 import createElement from "../utils/create-elem";
+import store from "../store/index";
 
 export default function Textarea(placeholder: string) {
+  const handleInput = (evt: Event) => {
+    const value = (evt.target as any).value;
+    store.dispatch({ type: "calculate", data: value });
+  };
+
   const textarea = createElement(
     "textarea",
-    { className: ["textarea"], placeholder },
+    {
+      className: ["textarea"],
+      placeholder,
+      events: {
+        type: "input",
+        action: handleInput,
+      },
+    },
     null
   );
 
